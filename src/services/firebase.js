b9838f655f005a3edb1bcedd9265b3268bdda11c
Rafael Calhau <rafael@calhau.me>
2020-07-04 23:59:55 -0300

    Created 2 methods on firebase service file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,6 +2,50 @@ import auth from '@react-native-firebase/auth'
 import database from '@react-native-firebase/database'
 import { signout } from '../store/actions'
 
+export const addEntryToContactList = async (userUid, contactUid) => {
+  const data = await database()
+    .ref(`/users/${userUid}`)
+    .once('value')
+    .then(snapshot => snapshot.val())
+
+  const ref = await database().ref(`/users/${userUid}`)
+
+  if (!data.contacts) {
+    // create list
+    return ref.set({ ...data, contacts: [contactUid] })
+      .catch(error => console.tron('[firebase]: addEntryToContactList error on create', error))
+  } else {
+    // update list
+    if (data.contacts.indexOf(contactUid) === -1) {
+      const newList = [...data.contacts, contactUid]
+      return ref.set({ ...data, contacts: newList })
+        .catch(error => console.tron('[firebase]: addEntryToContactList error on update', error))
+    }
+
+    return null
+  }
+}
+
+export const createContact = async (name, email, password) => {
+  const { user } = await auth().createUserWithEmailAndPassword(email, password)
+
+  if (user && user.uid) {
+    const userData = {
+      accountType: 'alumni',
+      name
+    }
+
+    await database()
+      .ref(`/users/${user.uid}`)
+      .set(userData)
+      .catch(error => console.tron('[firebase]: createContact error', error))
+
+    return user.uid
+  }
+
+  return null
+}
+
 export const getContacts = async (uid) => {
   return database()
     .ref(`/users/${uid}/contacts`)
@@ -35,10 +79,6 @@ export const registerProfile = async (uid, data) => {
   return ref
     .set(userData)
     .catch(error => console.tron('[firebase]: registerProfile error', error))
-}
-
-export const updateProfile = async (data) => {
-  return auth().currentUser.updateProfile(data)
 }
 
 export const signin = async (email, password) => {
