2b159df59fbb57ff237ee5a31d27278b58715d04
Rafael Calhau <rafael@calhau.me>
2020-07-05 00:00:29 -0300

    Implemented the new contact process

--- src/screens/Chats/index.js
+++ src/screens/Chats/index.js
@@ -1,14 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Keyboard } from 'react-native'
+import { Modal, Snackbar } from 'react-native-paper'
+import { SearchBox } from '../../components'
+import { Loader } from '../../components/SharedStyled'
+import { isValidEmail } from '../../modules/utils'
 import { store } from '../../store'
 import {
-  Body, ButtonLogout, Container, Header, HeaderBody,
-  HeaderIcon, Title, UserName, UserType
+  AvatarAlumini, Body, ButtonLogout, ButtonSubmitNewContact, ContactForm,
+  ContactFormTitle, Container, ButtonAddContact, Header, HeaderBody,
+  HeaderIcon, Input, Title, UserName, UserType, ZeroContactsText,
+  ZeroContactsView
 } from './styled'
-import { logout } from '../../services/firebase'
+import {
+  addEntryToContactList, createContact,
+  logout, getContacts
+} from '../../services/firebase'
 import { User as UserStorage } from '../../services/localstorage'
 
 function Chats ({ navigation }) {
   const { dispatch, state: { user } } = useContext(store)
+  const [alumniName, setAlumniName] = useState('Isa Calhau Monte')
+  const [alumniEmail, setAlumniEmail] = useState('isaiannyam@gmail.com')
+  const [alumniPassword, setAlumniPassword] = useState('123123')
+  const [alumniPasswordConfirmation, setAlumniPasswordConfirmation] = useState('123123')
+  const [contacts, setContacts] = useState('')
+  const [contactsLoaded, setContactsLoaded] = useState(false)
+  const [isAddingContact, setIsAddingContact] = useState(false)
+  const [modalIsActive, setModalIsActive] = useState(false)
+  const [searchKeywords, setSearchKeywords] = useState('')
+  const [snackbar, handleSnackbar] = useState({ visible: false, text: '' })
+
+  function handleDismissSnackBar () {
+    handleSnackbar({ visible: false, text: '' })
+  }
 
   function handleLogout () {
     logout(dispatch)
@@ -17,25 +41,160 @@ function Chats ({ navigation }) {
     navigation.navigate('SignIn')
   }
 
+  function handleNewContact () {
+    setModalIsActive(true)
+  }
+
+  async function handleNewContactSubmit () {
+    if (!alumniName.length) {
+      setSnack('Please enter the Name')
+    } else if (!isValidEmail(alumniEmail)) {
+      setSnack('Please enter a valid Email Address')
+    } else if (alumniPassword.length < 6) {
+      setSnack('Your password must have atleast 6 characters.')
+    } else if (alumniPasswordConfirmation !== alumniPassword) {
+      setSnack('Your password confirmation failed.')
+    } else {
+      setIsAddingContact(true)
+
+      const contactUid = await createContact(alumniName, alumniEmail, alumniPassword)
+
+      if (contactUid) {
+        // register on teacher contact list
+        await addEntryToContactList(user.uid, contactUid)
+        setContacts(contacts => [...contacts, {
+          name: alumniName
+        }])
+      }
+
+      resetNewContactForm()
+      setIsAddingContact(false)
+    }
+  }
+
+  function hideModal () {
+    if (!isAddingContact) {
+      setModalIsActive(false)
+    }
+  }
+
+  function renderContacts () {
+    if (!contacts.length) {
+      return (
+        <ZeroContactsView>
+          <AvatarAlumini />
+          <ZeroContactsText>Your contact list is empty</ZeroContactsText>
+        </ZeroContactsView>
+      )
+    }
+
+    return null
+  }
+
+  function resetNewContactForm () {
+    setAlumniName('')
+    setAlumniEmail('')
+    setAlumniPassword('')
+    setAlumniPasswordConfirmation('')
+  }
+
+  function setSnack (text = '') {
+    handleSnackbar({ visible: true, text })
+  }
+
+  // onMount
+  useEffect(() => {
+    const loadContacts = async () => {
+      const contacts = (await getContacts(user.uid)) || []
+
+      setContacts(contacts)
+      setContactsLoaded(true)
+    }
+
+    loadContacts()
+  }, []) // eslint-disable-line
+
   return (
-    <Container>
-      <Header>
-        <HeaderIcon name='user' />
+    <>
+      <Container>
+        <Header>
+          <HeaderIcon name='user' />
 
-        <HeaderBody>
-          <UserName>{user.name}</UserName>
-          <UserType>{user.accountType}</UserType>
-        </HeaderBody>
+          <HeaderBody>
+            <UserName>{user.name}</UserName>
+            <UserType>{user.accountType}</UserType>
+          </HeaderBody>
 
-        <ButtonLogout onPress={handleLogout}>
-          <HeaderIcon name='logout' />
-        </ButtonLogout>
-      </Header>
+          <ButtonLogout onPress={handleLogout}>
+            <HeaderIcon name='logout' />
+          </ButtonLogout>
+        </Header>
 
-      <Body>
-        <Title>Chats</Title>
-      </Body>
-    </Container>
+        <Body>
+          <Title>Chats</Title>
+          <SearchBox
+            autoCapitalize='none'
+            autoCorrect={false}
+            onChangeText={setSearchKeywords}
+            placeholder='Search alumnis...'
+            value={searchKeywords}
+          />
+
+          {
+            !contactsLoaded
+              ? <Loader />
+              : renderContacts()
+          }
+
+          <ButtonAddContact
+            icon='plus'
+            onPress={handleNewContact}
+          />
+
+          <Snackbar
+            visible={snackbar.visible}
+            onDismiss={handleDismissSnackBar}
+          >
+            {snackbar.text}
+          </Snackbar>
+        </Body>
+      </Container>
+
+      <Modal visible={modalIsActive} onDismiss={hideModal}>
+        <ContactForm onTouchStart={() => Keyboard.dismiss()}>
+          <ContactFormTitle>Add new alumni</ContactFormTitle>
+
+          <Input
+            label='Name'
+            onChangeText={setAlumniName}
+            value={alumniName}
+          />
+          <Input
+            label='Email'
+            onChangeText={setAlumniEmail}
+            value={alumniEmail}
+          />
+          <Input
+            secureTextEntry
+            label='Password'
+            onChangeText={setAlumniPassword}
+            value={alumniPassword}
+          />
+          <Input
+            secureTextEntry
+            label='Confirm Password'
+            onChangeText={setAlumniPasswordConfirmation}
+            value={alumniPasswordConfirmation}
+          />
+
+          <ButtonSubmitNewContact
+            label='Add'
+            loading={isAddingContact}
+            onPress={handleNewContactSubmit}
+          />
+        </ContactForm>
+      </Modal>
+    </>
   )
 }
 
--- src/screens/Chats/styled.js
+++ src/screens/Chats/styled.js
@@ -1,25 +1,59 @@
 import styled from 'styled-components/native'
 import AntIcons from 'react-native-vector-icons/AntDesign'
+import { FAB } from 'react-native-paper'
+import { Button, InputText } from '../../components'
 import { Text } from '../../components/SharedStyled'
 
+export const AvatarAlumini = styled.Image.attrs({
+  source: require('../../assets/avatar-alumni-large.png')
+})`
+  align-self: center;
+  margin-bottom: 25px
+`
+
 export const Body = styled.View`
-  align-self: stretch;
   flex: 1
 `
+
+export const ButtonAddContact = styled(FAB).attrs({
+  color: 'white'
+})`
+  background-color: ${({ theme }) => theme.colors.primary};
+  position: absolute;
+  right: 20px
+  bottom: 20px
+`
+
+export const ButtonSubmitNewContact = styled(Button)`
+  margin-top: 20px
+`
+
 export const ButtonLogout = styled.TouchableOpacity`
   height: 40px;
   width: 40px;
 `
 
+export const ContactForm = styled.View`
+  align-self: center;
+  background-color: white;
+  width: 80%;
+  padding: 0 30px 30px;
+`
+
+export const ContactFormTitle = styled(Text)`
+  color: ${({ theme }) => theme.colors.dark};
+  font-size: 22px;
+  font-weight: 600;
+  margin: 20px 0 30px
+`
+
 export const Container = styled.View`
-  align-self: stretch;
   flex: 1;
   padding: 0 20px;
 `
 
 export const Header = styled.View`
   align-items: center;
-  align-self: stretch;
   flex-direction: row;
   height: 82px;
   margin-bottom: 25px
@@ -28,14 +62,21 @@ export const Header = styled.View`
 export const HeaderBody = styled.View`
   flex: 1;
   flex-direction: column;
-  height: 82px;
   justify-content: center;
   padding: 0 20px
 `
 
+export const Input = styled(InputText).attrs({
+  autoCapitalize: 'none',
+  autoCorrect: false
+})`
+  margin: ${({ last }) => !last ? '15px 0 0 0' : '15px 0'}
+`
+
 export const Title = styled(Text)`
   color: ${({ theme }) => theme.colors.dark};
-  font-size: 24px
+  font-size: 28px;
+  font-weight: bold;
 `
 
 export const HeaderIcon = styled(AntIcons).attrs({
@@ -52,4 +93,18 @@ export const UserName = styled(Text)`
 export const UserType = styled(Text)`
   color: ${({ theme }) => theme.colors.darkgray};
   font-size: 16px
+`
+
+export const ZeroContactsText = styled(Text)`
+  color: ${({ theme }) => theme.colors.darkgray};
+  margin-bottom: 25px;
+  font-size: 18px;
+  font-weight: 300
+`
+
+export const ZeroContactsView = styled.View`
+  align-items: center;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
 `
